31716e3a370b99daa0f32a82709893cbd7cd94f4
ICHIKAWA Kento <kent@tahoiya.org>
2022-12-28 13:57:31 +0900

    added some tests to adder/src/lib.rs

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,20 @@
+pub fn greeting(name: &str) -> String {
+    format!("Hello")
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 #[cfg(test)]
@@ -10,5 +25,40 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn lager_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 9,
+            height: 6,
+        };
+        let smaller = Rectangle {
+            width: 4,
+            height: 2,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 }
